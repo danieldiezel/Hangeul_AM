ccdac02d19d59be4f766ceb76ad9bf605e67326c
diezel.daniel <daniel.bestellungen@web.de>
2021-12-25 01:25:49 +0100

    HAN-11 small technical fixes

--- ressources/common/main.js
+++ ressources/common/main.js
@@ -2,8 +2,7 @@ var levelTwoLearnBarScore = document.getElementById("levelTwoLearnBar");
 var levelTwoLearnPercentage
 
 window.onload = function() {
-    levelTwoLearnPercentage = `${(localStorage.getItem('levelTwoLearnScore')/26) * 100}`;
-    console.log(levelTwoLearnPercentage);
-    document.getElementById("progressBarTwoLearnFull").innerHTML += Math.round(`${(localStorage.getItem('levelTwoLearnScore')/26) * 100}`) + "%" ;
+    levelTwoLearnPercentage = `${(localStorage.getItem('levelTwoLearnScore')/25) * 100}`;
+    document.getElementById("progressBarTwoLearnFull").innerHTML += Math.round(`${(localStorage.getItem('levelTwoLearnScore')/25) * 100}`) + "%" ;
     document.getElementById('progressBarTwoLearnFull').style.width = levelTwoLearnPercentage + "%";
 }
