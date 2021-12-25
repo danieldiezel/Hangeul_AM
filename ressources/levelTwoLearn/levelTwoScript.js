a5feb539f35d05bb69986cef87b0ec5cf6082178
diezel.daniel <daniel.bestellungen@web.de>
2021-12-25 01:27:10 +0100

    HAN-26 add flags for countries and logic for button, small fixes

--- ressources/levelTwoLearn/levelTwoScript.js
+++ ressources/levelTwoLearn/levelTwoScript.js
@@ -5,53 +5,57 @@ var showGerman = document.getElementById("showGerman");
 var menu = document.getElementById("menu");
 var koreanImage = document.getElementById("koreanImage");
 var progressBarFull = document.getElementById("progressBarFull")
+var showGermanText = document.getElementById("germanPart");
+var germanMeaning = document.getElementById("germanMeaning");
 var koreanPicture;
 var step;
-const maxCountries = 26;
+const maxCountries = 25;
 const score = [];
 const scoreOne = [];
 
 // TODO wenn spiel schon gespielt progress bar hinstellen uaf den score
 window.onload = function() {
+    further.style.display = "none";
+    germanMeaning.style.display = "none";
     if (localStorage.getItem('levelTwoLearnScore') != null) {
         score.push(localStorage.getItem('levelTwoLearnScore'));
-        console.log(score);
         alreadyDone = score[0];
-        console.log(alreadyDone);
         for (var step = 1; step < alreadyDone; step++) {
             score.push("1");
         }
-        console.log(score);
     }   
 }
 
 showGerman.onclick = function() {
     further.style.display = "block";
+    showGermanText.style.display = "none";
+    germanMeaning.style.display = "block";
 }
 
 further.onclick = function() {
     further.style.display = "none";
+    germanMeaning.style.display = "none";
     if (score.length < 25) {
         score.push("1");
         localStorage.setItem('levelTwoLearnScore', score.length);
+        showGermanText.style.display = "block";
         nextCountry();
         progressBar();
     } else {
         // TODO meldung einbauen das man das Quiz freigeschaltet hat.
         if (scoreOne.length < 25) {
-            console.log(scoreOne.length);
             scoreOne.push("1");
             nextCountryTwo();
+            showGermanText.style.display = "block";
         } else {
             scoreOne.length = 0;
-            console.log(scoreOne);
             nextCountryTwo();
+            showGermanText.style.display = "block";
         }
     }    
 }
 
 function progressBar() {
-    console.log(`${(score.length/maxCountries) * 100}%`);
     document.getElementById("progressBarFull").style.width = `${(score.length/maxCountries) * 100}%`;
 }
 
