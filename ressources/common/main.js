03f1898b50dcb4526ba6afa2a2437752e377645e
diezel.daniel <daniel.bestellungen@web.de>
2021-12-31 17:02:47 +0100

    HAN-36 levelTwoQuiz is not clickable until you reach 100 percent in levelTwoLearn

--- ressources/common/main.js
+++ ressources/common/main.js
@@ -6,12 +6,19 @@ var closeSettings = document.getElementById("closeSettingsButton");
 var settingsContainer = document.getElementById("settingsContainer");
 var normalMenu = document.getElementById("menuContainer");
 var resetScoreButton = document.getElementById("resetScoreButton");
+var levelTwoQuizButton = document.getElementsByName("levelTwoQuizButton");
 
 window.onload = function() {
     levelTwoLearnPercentage = `${(localStorage.getItem('levelTwoLearnScore')/25) * 100}`;
     document.getElementById("progressBarTwoLearnFull").innerHTML += Math.round(`${(localStorage.getItem('levelTwoLearnScore')/25) * 100}`) + "%" ;
     document.getElementById('progressBarTwoLearnFull').style.width = levelTwoLearnPercentage + "%";
     levelTwoQuizScore = localStorage.getItem('levelTwoQuizScore');
+    if (levelTwoLearnPercentage != 100) {
+        document.levelTwoQuizButton.action = "javascript:void(0)";
+    } else {
+        document.levelTwoQuizButton.action = "/ressources/levelTwoQuiz/levelTwoQuiz.html";
+    }
+    
 }
 
 openSettings.onclick = function() {
